afdf61d97b54a43b0a98b53efcdedd04963eba96
호균 임 <jellyho@o365.yonsei.ac.kr>
2023-07-29 15:18:30 +0900

    sqlupdate error fixed

--- example_sqlUpdate.py
+++ example_sqlUpdate.py
@@ -1,5 +1,8 @@
 from data.updaters import SQLUpdater
 
-updater = SQLUpdater(host='jellyho.iptime.org', user='yammyquant', password='dialfl752', db='binance')
-updater.setTable('BTCUSDT', ['1w', '1d', '6h', '1h', '15m', '5m', '1m'])
-updater.update()
+updater1 = SQLUpdater(host='jellyho.iptime.org', user='yammyquant', password='dialfl752', db='binance')
+updater1.setTable('BTCUSDT', ['1w', '1d', '6h', '1h', '15m', '5m', '1m'])
+updater2 = SQLUpdater(host='jellyho.iptime.org', user='yammyquant', password='dialfl752', db='binance')
+updater2.setTable('BTCETH', ['1w', '1d', '6h', '1h', '15m', '5m', '1m'])
+updater1.update()
+updater2.update()
