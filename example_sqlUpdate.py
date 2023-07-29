5d28c929aa7778d08eefd7c9b0033bf52e1b503f
호균 임 <jellyho@o365.yonsei.ac.kr>
2023-07-29 15:19:11 +0900

    sqlupdate error fixed

--- example_sqlUpdate.py
+++ example_sqlUpdate.py
@@ -3,6 +3,6 @@ from data.updaters import SQLUpdater
 updater1 = SQLUpdater(host='jellyho.iptime.org', user='yammyquant', password='dialfl752', db='binance')
 updater1.setTable('BTCUSDT', ['1w', '1d', '6h', '1h', '15m', '5m', '1m'])
 updater2 = SQLUpdater(host='jellyho.iptime.org', user='yammyquant', password='dialfl752', db='binance')
-updater2.setTable('BTCETH', ['1w', '1d', '6h', '1h', '15m', '5m', '1m'])
+updater2.setTable('ETHUSDT', ['1w', '1d', '6h', '1h', '15m', '5m', '1m'])
 updater1.update()
 updater2.update()
