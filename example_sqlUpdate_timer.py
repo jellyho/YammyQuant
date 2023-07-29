518f2059cb8025dc450cb8ba9464b133ac8225ed
호균 임 <jellyho@o365.yonsei.ac.kr>
2023-07-29 15:19:43 +0900

    ETHUSDT update

--- example_sqlUpdate_timer.py
+++ example_sqlUpdate_timer.py
@@ -2,12 +2,15 @@ from data.updaters import SQLUpdater
 import time
 
 
-updater = SQLUpdater(host='jellyho.iptime.org', user='yammyquant', password='dialfl752', db='binance')
-updater.setTable('BTCUSDT', ['1w', '1d', '6h', '1h', '15m', '5m', '1m'])
+updater1 = SQLUpdater(host='jellyho.iptime.org', user='yammyquant', password='dialfl752', db='binance')
+updater1.setTable('BTCUSDT', ['1w', '1d', '6h', '1h', '15m', '5m', '1m'])
+updater2 = SQLUpdater(host='jellyho.iptime.org', user='yammyquant', password='dialfl752', db='binance')
+updater2.setTable('ETHUSDT', ['1w', '1d', '6h', '1h', '15m', '5m', '1m'])
 
 while True:
     try:
-        updater.update()
+        updater1.update()
+        updater2.update()
     except:
         print('error occurred')
     time.sleep(3600 * 6)
